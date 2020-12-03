8bc094fe0eefef5e4beb74c90b3f093de9527653
milanaadams <milanaadams@gmail.com>
2020-12-04 00:20:53 +0300

    fix: find processing error

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -128,9 +128,11 @@ function getAngleBetweenVectors(x1, y1, x2, y2) {
  *     5     => 5
  *     0     => 0
  */
-function getLastDigit(value) {
-  const valueStr = value.toString();
-  return +(valueStr[valueStr.length - 1]);
+function getLastDigit(/* value */) {
+  /* const valueStr = value.toString();
+  return +(valueStr[valueStr.length - 1]); */
+
+  throw new Error('Not implemented');
 }
 
 
@@ -145,8 +147,9 @@ function getLastDigit(value) {
  *     '37'     => 37
  * '-525.5'     => -525.5
  */
-function parseNumberFromString(value) {
-  return +value;
+function parseNumberFromString(/* value */) {
+  /* return +value; */
+  throw new Error('Not implemented');
 }
 
 /**
@@ -162,8 +165,9 @@ function parseNumberFromString(value) {
  *   3,3,3   => 5.196152422706632
  *   1,2,3   => 3.741657386773941
  */
-function getParallelipidedDiagonal(a, b, c) {
-  return Math.sqrt(a ** 2 + b ** 2 + c ** 2);
+function getParallelipidedDiagonal(/* a, b, c */) {
+  /* return Math.sqrt(a ** 2 + b ** 2 + c ** 2); */
+  throw new Error('Not implemented');
 }
 
 /**
@@ -183,8 +187,9 @@ function getParallelipidedDiagonal(a, b, c) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(num, pow) {
-  return (Math.round(num / 10 ** pow)) * 10 ** pow;
+function roundToPowerOfTen(/* num, pow */) {
+  /* return (Math.round(num / 10 ** pow)) * 10 ** pow; */
+  throw new Error('Not implemented');
 }
 
 /**
@@ -204,11 +209,12 @@ function roundToPowerOfTen(num, pow) {
  *   16 => false
  *   17 => true
  */
-function isPrime(n) {
-  for (let i = Math.floor(n / 2); i > 1; i -= 1) {
+function isPrime(/* n */) {
+  /* for (let i = Math.floor(n / 2); i > 1; i -= 1) {
     if (n % i === 0) return false;
   }
-  return true;
+  return true; */
+  throw new Error('Not implemented');
 }
 
 /**
@@ -226,8 +232,9 @@ function isPrime(n) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(value, def) {
-  return +value || def;
+function toNumber(/* value, def */) {
+  /* return +value || def; */
+  throw new Error('Not implemented');
 }
 
 module.exports = {
